9d2696ee214403a58ef9184324bf4b3fb1324009
MuhabGamal <muhabme@gmail.com>
2024-02-20 12:58:37 +0200

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+
+#include "sort.h"
+
+/**
+ * partition - Array partition
+ * @array: The array to sort
+ * @first: First position
+ * @last: Last position
+ * @size: Array size
+ * Returns: int pivot index
+ */
+int partition(int *array, int first, int last, size_t size)
+{
+	int i = first - 1, aux, j;
+
+	for (j = first; j <= last - 1; j++)
+	{
+		if (array[j] < array[last])
+		{
+			i++;
+			if (i < j)
+			{
+				aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > array[last])
+	{
+		aux = array[i + 1];
+		array[i + 1] = array[last];
+		array[last] = aux;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * qs - Sorts an array of integers recursively
+ * @array: The array to sort
+ * @first: First position
+ * @last: Last position
+ * @size: Array size
+ */
+void qs(int *array, int first, int last, size_t size)
+{
+	int pivot;
+
+	if (first < last)
+	{
+		pivot = partition(array, first, last, size);
+		qs(array, first, pivot - 1, size);
+		qs(array, pivot + 1, last, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers using the Quick
+ * sort algorithm in ascending order
+ * @array: The array to sort
+ * @size: Array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	qs(array, 0, size - 1, size);
+}
