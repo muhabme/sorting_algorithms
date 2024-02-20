3b33d1378af2cc2b98265532a0e222026820b4d5
MuhabGamal <muhabme@gmail.com>
2024-02-20 17:33:50 +0200

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+*swap - Positions of two elements into an array
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+void swap(int *array, int item1, int item2)
+{
+
+	int tmp;
+
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
+}
+/**
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
+ * @size: size of array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, index = 0;
+
+	if (array == NULL || size < 2)
+		return;
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+			for (index = i; index >= gap &&
+			 (array[index] < array[index - gap]); index -= gap)
+				swap(array, index, index - gap);
+		print_array(array, size);
+		gap /= 3;
+	}
+}
